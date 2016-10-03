12338266ace13fe00fa26e35a66f4e306d383e2b
Benjamin <bsterner6696@gmail.com>
2016-10-03 17:16:41 -0500

    Added class holding chat room, and methods for connecting of new users.

---
+++ TCPChatRoomProject/TCPChatRoomProjectServerSide/ChatRoom.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using System.Threading;
+using Chat = System.Net;
+
+
+namespace TCPChatRoomProjectServerSide
+{
+    public class ChatRoom
+    {
+        Reader reader = new Reader();
+        Writer writer = new Writer();
+        System.Net.Sockets.TcpClient client;
+
+        public ChatRoom(System.Net.Sockets.TcpClient tcpClient, UserDictionary dictionary, Server server)
+        {
+            client = tcpClient;
+
+            Thread chatThread = new Thread(() => EnterChat(client, dictionary, server));
+            chatThread.Start();
+        }
+        private string GetNickName(System.Net.Sockets.TcpClient client)
+        {
+            writer.writer = new StreamWriter(client.GetStream());
+            reader.reader = new StreamReader(client.GetStream());
+            writer.writer.WriteLine("What's your name? ");
+            writer.writer.Flush();
+            return reader.reader.ReadLine();
+        }
+
+        public void EnterChat(System.Net.Sockets.TcpClient client, UserDictionary dictionary, Server server)
+        {
+            reader.reader = new StreamReader(client.GetStream());
+            writer.writer = new StreamWriter(client.GetStream());
+            writer.writer.WriteLine("You're a real bugel boy.");
+            string nickName = GetNickName(client);
+            while (dictionary.ClientsByName.ContainsKey(nickName))
+            {
+                writer.writer.WriteLine("Pick a different name.");
+                nickName = GetNickName(client);
+            }
+            dictionary.ClientsByName.Add(nickName, client);
+            dictionary.ClientsByNumber.Add(client, nickName);
+
+        }
+    }
+}
--- TCPChatRoomProject/TCPChatRoomProjectServerSide/Server.cs
+++ TCPChatRoomProject/TCPChatRoomProjectServerSide/Server.cs
@@ -25,15 +25,12 @@ namespace TCPChatRoomProjectServerSide
             {
                 try
                 {
-                    writer.writer = new StreamWriter(clients[number].GetStream());
-                    writer.writer.WriteLine("{0}: {1}", nickName, message);
-                    writer.writer.Flush();
-                    writer = null;
+                    SendMessage(clients[number], nickName, message);
                 }
                 catch 
                 {
                     string userName = userDictionary.ClientsByNumber[clients[number]];
-                    SendSystemMessage(userName + " has left the chat.");
+                    SendSystemMessageToAll(userName + " has left the chat.");
                     userDictionary.ClientsByName.Remove(userDictionary.ClientsByNumber[clients[number]]);
                     userDictionary.ClientsByNumber.Remove(clients[number]);
 
@@ -41,7 +38,15 @@ namespace TCPChatRoomProjectServerSide
             }
         }
 
-        public void SendSystemMessage(string message)
+        public void SendMessage(Chat.Sockets.TcpClient client, string nickName, string message)
+        {
+            writer.writer = new StreamWriter(client.GetStream());
+            writer.writer.WriteLine("{0}: {1}", nickName, message);
+            writer.writer.Flush();
+            writer = null;
+        }
+
+        public void SendSystemMessageToAll(string message)
         {
             Chat.Sockets.TcpClient[] clients = new Chat.Sockets.TcpClient[userDictionary.ClientsByName.Count];
             userDictionary.ClientsByName.Values.CopyTo(clients, 0);
@@ -49,15 +54,33 @@ namespace TCPChatRoomProjectServerSide
             {
                 try
                 {
-                    writer.writer = new StreamWriter(clients[number].GetStream());
-                    writer.writer.Write(message);
-                    writer.writer.Flush();
-                    writer = null;
+                    SendSystemMessage(clients[number], message);
+
                 }
                 catch
                 {
                     userDictionary.ClientsByName.Remove(userDictionary.ClientsByNumber[clients[number]]);
                     userDictionary.ClientsByNumber.Remove(clients[number]);
+                }
+            }
+        }
+        public void SendSystemMessage(Chat.Sockets.TcpClient client, string message)
+        {
+            writer.writer = new StreamWriter(client.GetStream());
+            writer.writer.Write(message);
+            writer.writer.Flush();
+            writer = null;
+        }
+
+        public void ConnectClient()
+        {
+            while (true)
+            {
+                listener.serverListener.Start();
+                if (listener.serverListener.Pending())
+                {
+                    Chat.Sockets.TcpClient connectedClient = listener.serverListener.AcceptTcpClient();
+                    
                 }
             }
         }
