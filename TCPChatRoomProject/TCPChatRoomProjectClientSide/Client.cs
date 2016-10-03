597763ff2ca9e3e05033304a020cb0eb2ae0b192
Benjamin <bsterner6696@gmail.com>
2016-10-03 18:03:43 -0500

    rudimentary client program made

---
+++ TCPChatRoomProject/TCPChatRoomProjectClientSide/Client.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Threading;
+using System.Net;
+using Chat = System.Net;
+
+namespace TCPChatRoomProjectClientSide
+{
+    class Client
+    {
+        System.Net.Sockets.TcpClient client = new System.Net.Sockets.TcpClient("10.2.20.18", 8000);
+        StreamReader reader;
+        StreamWriter writer;
+
+        public void ConnectToServer()
+        {
+            reader = new StreamReader(client.GetStream());
+            writer = new StreamWriter(client.GetStream());
+
+        }
+
+        public void WriteMessage()
+        {
+            string line;
+            line = Console.ReadLine();
+            writer.WriteLine(line);
+        }
+        public void RunClient()
+        {
+            ConnectToServer();
+            while (true)
+            {
+                WriteMessage();
+            }
+        }
+    }
+}
