8f39947ee1b1769d28c032cf2d2466284c955875
Benjamin <bsterner6696@gmail.com>
2016-10-03 14:01:15 -0500

    Started serverside project, created class containing dictionaries to hold user list

---
+++ TCPChatRoomProject/TCPChatRoomProjectServerSide/userDictionary.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using System.Threading;
+using Chat = System.Net;
+
+
+namespace TCPChatRoomProjectServerSide
+{
+    public class userDictionary
+    {
+        public Dictionary<string, Chat.Sockets.TcpClient> ClientsByName = new Dictionary<string, Chat.Sockets.TcpClient>();
+        public Dictionary<Chat.Sockets.TcpClient, string> ClientsByNumber = new Dictionary<Chat.Sockets.TcpClient, string>(); 
+    }
+}
